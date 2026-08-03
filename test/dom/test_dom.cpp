e0006a7875d6255d2eb38e074129d6acf7f6b943
Ronald Tse <ronald.tse@ribose.com>
2026-08-03 22:15:09 +0800

    test: specs for previously-uncovered public APIs
    Adds 8 specs covering public API functions that had no test
    coverage:
    
    Node content accessors:
    - CdataNodeReturnsContent (taurus_cdata_node_get_content)
    - CommentNodeReturnsContent (taurus_comment_node_get_content)
    
    Attribute type accessors:
    - IntAttributeReturnsValue (taurus_element_attribute_int)
    - DoubleAttributeReturnsValue (taurus_element_attribute_double)
    
    Text content type accessors:
    - IntTextReturnsValue (taurus_element_text_int)
    - DoubleTextReturnsValue (taurus_element_text_double)
    
    Serialization + mutation:
    - RoundTripPreservesStructure (taurus_document_serialize)
    - AppendChildMovesElement (taurus_element_append_child)
    
    Test count: 155 → 163. All pass under both regular and ASAN builds.

--- test/dom/test_dom.cpp
+++ test/dom/test_dom.cpp
@@ -121,3 +121,134 @@ TEST(DocumentFreeze, NullDocReturnsSafe) {
     EXPECT_EQ(taurus_document_freeze(nullptr), TAURUS_ERROR_NULL_ARG);
     EXPECT_EQ(taurus_document_is_frozen(nullptr), 0);
 }
+
+// ---- Node content accessors (TODO: specs coverage) -----------------------
+
+TEST(NodeContentAccessors, CdataNodeReturnsContent) {
+    TaurusStatus st = TAURUS_OK;
+    const char xml[] = "<r><![CDATA[raw <content> & stuff]]></r>";
+    TaurusDocument doc = taurus_parse_string(xml, std::strlen(xml), &st);
+    ASSERT_NE(doc, nullptr);
+
+    TaurusElement root = taurus_document_root(doc);
+    TaurusNodeRef child = taurus_node_first_child((TaurusNodeRef)root);
+    ASSERT_NE(child, nullptr);
+
+    const char* cdata = taurus_cdata_node_get_content(child);
+    EXPECT_NE(cdata, nullptr);
+    EXPECT_STREQ(cdata, "raw <content> & stuff");
+
+    taurus_document_free(doc);
+}
+
+TEST(NodeContentAccessors, CommentNodeReturnsContent) {
+    TaurusStatus st = TAURUS_OK;
+    const char xml[] = "<r><!-- a comment --></r>";
+    TaurusDocument doc = taurus_parse_string(xml, std::strlen(xml), &st);
+    ASSERT_NE(doc, nullptr);
+
+    TaurusElement root = taurus_document_root(doc);
+    TaurusNodeRef child = taurus_node_first_child((TaurusNodeRef)root);
+    ASSERT_NE(child, nullptr);
+
+    const char* comment = taurus_comment_node_get_content(child);
+    EXPECT_NE(comment, nullptr);
+    EXPECT_STREQ(comment, " a comment ");
+
+    taurus_document_free(doc);
+}
+
+// ---- Attribute type accessors -------------------------------------------
+
+TEST(AttributeAccessors, IntAttributeReturnsValue) {
+    TaurusStatus st = TAURUS_OK;
+    const char xml[] = "<r count='42'/>";
+    TaurusDocument doc = taurus_parse_string(xml, std::strlen(xml), &st);
+    ASSERT_NE(doc, nullptr);
+    TaurusElement root = taurus_document_root(doc);
+    EXPECT_EQ(taurus_element_attribute_int(root, "count", 0), 42);
+    EXPECT_EQ(taurus_element_attribute_int(root, "missing", 99), 99);
+    taurus_document_free(doc);
+}
+
+TEST(AttributeAccessors, DoubleAttributeReturnsValue) {
+    TaurusStatus st = TAURUS_OK;
+    const char xml[] = "<r pi='3.14'/>";
+    TaurusDocument doc = taurus_parse_string(xml, std::strlen(xml), &st);
+    ASSERT_NE(doc, nullptr);
+    TaurusElement root = taurus_document_root(doc);
+    EXPECT_DOUBLE_EQ(taurus_element_attribute_double(root, "pi", 0.0), 3.14);
+    taurus_document_free(doc);
+}
+
+// ---- Text content type accessors ----------------------------------------
+
+TEST(TextContentAccessors, IntTextReturnsValue) {
+    TaurusStatus st = TAURUS_OK;
+    const char xml[] = "<r>123</r>";
+    TaurusDocument doc = taurus_parse_string(xml, std::strlen(xml), &st);
+    ASSERT_NE(doc, nullptr);
+    TaurusElement root = taurus_document_root(doc);
+    EXPECT_EQ(taurus_element_text_int(root, 0), 123);
+    taurus_document_free(doc);
+}
+
+TEST(TextContentAccessors, DoubleTextReturnsValue) {
+    TaurusStatus st = TAURUS_OK;
+    const char xml[] = "<r>45.67</r>";
+    TaurusDocument doc = taurus_parse_string(xml, std::strlen(xml), &st);
+    ASSERT_NE(doc, nullptr);
+    TaurusElement root = taurus_document_root(doc);
+    EXPECT_DOUBLE_EQ(taurus_element_text_double(root, 0.0), 45.67);
+    taurus_document_free(doc);
+}
+
+// ---- Serialize document ---------------------------------------------------
+
+TEST(SerializeDocument, RoundTripPreservesStructure) {
+    TaurusStatus st = TAURUS_OK;
+    const char xml[] = "<root><child>text</child></root>";
+    TaurusDocument doc = taurus_parse_string(xml, std::strlen(xml), &st);
+    ASSERT_NE(doc, nullptr);
+
+    char* output = taurus_document_serialize(doc, nullptr);
+    EXPECT_NE(output, nullptr);
+    if (output) {
+        /* The serialized output should contain the element names. */
+        std::string s(output);
+        EXPECT_NE(s.find("root"), std::string::npos);
+        EXPECT_NE(s.find("child"), std::string::npos);
+        EXPECT_NE(s.find("text"), std::string::npos);
+        taurus_free_string(output);
+    }
+    taurus_document_free(doc);
+}
+
+// ---- Element mutation -----------------------------------------------------
+
+TEST(ElementMutation, AppendChildMovesElement) {
+    TaurusStatus st = TAURUS_OK;
+    const char xml[] = "<r><a/><b/></r>";
+    TaurusDocument doc = taurus_parse_string(xml, std::strlen(xml), &st);
+    ASSERT_NE(doc, nullptr);
+
+    TaurusElement root = taurus_document_root(doc);
+    TaurusElement a = taurus_element_first_child_any(root);
+    ASSERT_NE(a, nullptr);
+    EXPECT_STREQ(taurus_element_name(a), "a");
+
+    TaurusElement b = taurus_element_next_sibling_any(a);
+    ASSERT_NE(b, nullptr);
+    EXPECT_STREQ(taurus_element_name(b), "b");
+
+    /* Move b under a. */
+    TaurusStatus rc = taurus_element_append_child(a, b);
+    EXPECT_EQ(rc, TAURUS_OK);
+
+    /* a should now have b as a child. */
+    TaurusElement child = taurus_element_first_child_any(a);
+    ASSERT_NE(child, nullptr);
+    EXPECT_STREQ(taurus_element_name(child), "b");
+
+    taurus_document_free(doc);
+}
